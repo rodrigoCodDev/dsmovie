42fe4bb93173faf2b39e72e5aed396b3f794ba39
Rodrigo <rodrigo.oliveira200310@gmail.com>
2022-01-15 14:05:39 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,25 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing(): any {
     return (
-        <Pagination />
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+
+                    {/* Quantidade de cards para cada dimensão de tela */}
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+
+
+                </div>
+            </div>
+
+        </>
     );
 }
 
