9129ec1cdb5c4dff6405b7428805014b9e45dde6
Rodrigo <rodrigo.oliveira200310@gmail.com>
2022-01-16 19:43:32 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,17 +1,31 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/request";
 
 function Listing(): any {
 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {
-            console.log(response)
-        })
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [pageNumber, setPageNumber] = useState(0)
+
+    useEffect(() => {
+        // page especifica o número da página
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+            .then(response => {
+                const data = response.data as MoviePage
+                console.log(data)
+                setPageNumber(data.number)
+            })
+
+    }, [])
+
 
     return (
+
         <>
+            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
