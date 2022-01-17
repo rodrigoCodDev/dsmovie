7680509e51db5d2621f6e2cb989980d18a23a9d2
Rodrigo <rodrigo.oliveira200310@gmail.com>
2022-01-16 21:30:49 -0300

    Adapting website

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,2 +1,3 @@
 // Caso não esteja definida use por padrão localhost (??)
+// REACT_APP_BACKEND_URL é uma variável do netlify
 export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080"
